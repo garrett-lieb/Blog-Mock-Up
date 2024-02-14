c75130caa2cab8be6211ad82d56da3465c3cdf34
garrett lieb <liebg08@gmail.com>
2024-02-14 15:52:09 -0800

    got POST for new blog to work, trying to figure out POST for comments, got routes made without breaking server connection

--- controllers/index.js
+++ controllers/index.js
@@ -6,10 +6,12 @@ const apiroutes = require('./api');
 router.use('/', homeroutes);
 router.use('/api', apiroutes);
 
+
+
 // log 404 when a route is not found
-router.use((req, res) => {
-  res.send('<h1>404!!!</h1>');
-});
+// router.use((req, res) => {
+//   res.send('<h1>404!!!</h1>');
+// });
 
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,12 +1,39 @@
 const User = require('./User');
 const Blog = require('./Blog');
-
+const Comment = require('./Comment');
 
 Blog.belongsTo(User, {
     foreignKey: 'userID',
     onDelete: 'CASCADE'
 });
 
+
+
+User.hasMany(Blog, {
+    foreignKey: 'userID',
+    onDelete: 'CASCADE'
+});
+
 // add associations here for comments
+
+Blog.hasMany(Comment, {
+    foreignKey: 'blog_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(Blog, {
+    foreignKey: 'blog_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+
 
 module.exports = { User, Blog };
