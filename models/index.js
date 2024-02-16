b65fda9dfe0bd76abcf377283ff118f2594a87db
garrett lieb <liebg08@gmail.com>
2024-02-15 17:41:57 -0800

    reordered models/index.js for clarity

--- models/index.js
+++ models/index.js
@@ -2,31 +2,32 @@ const User = require('./User');
 const Blog = require('./Blog');
 const Comment = require('./Comment');
 
-Blog.belongsTo(User, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
-});
-
-
 User.hasMany(Blog, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE'
 });
 
-// add associations here for comments
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Blog.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
 
 Blog.hasMany(Comment, {
     foreignKey: 'blog_id',
     onDelete: 'CASCADE'
 });
 
+
+// add associations here for comments
+
+
 Comment.belongsTo(Blog, {
     foreignKey: 'blog_id'
-});
-
-User.hasMany(Comment, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
 });
 
 Comment.belongsTo(User, {
