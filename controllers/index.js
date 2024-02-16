323b20815460faedf383bd41b8fb56014b5fd59a
garrett lieb <liebg08@gmail.com>
2024-02-15 16:31:39 -0800

    changing index.js to have a 404 route

--- controllers/index.js
+++ controllers/index.js
@@ -9,9 +9,9 @@ router.use('/api', apiroutes);
 
 
 // log 404 when a route is not found
-// router.use((req, res) => {
-//   res.send('<h1>404!!!</h1>');
-// });
+router.use((req, res) => {
+  res.send('<h1>404!!!</h1>');
+});
 
 
 module.exports = router;
