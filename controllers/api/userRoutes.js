7cc88892324c0945facaa8647c9cb40ec8463c74
garrett lieb <liebg08@gmail.com>
2024-02-22 18:24:49 -0800

    tree shake

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -50,7 +50,7 @@ router.delete('/:id', async (req, res) => {
     }
 });
 
-router.post ('/login', async (req, res) => {
+router.post('/login', async (req, res) => {
     try {
         const userData = await User.findOne({ where: { username: req.body.username } });
         if (!userData) {
--- public/js/login.js
+++ public/js/login.js
@@ -1,20 +1,20 @@
 const loginHandler = async (event) => {
     event.preventDefault();
-    
-    const userName= document.querySelector('#user-login').value.trim();
+
+    const userName = document.querySelector('#user-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
-    
+
     if (userName && password) {
         const response = await fetch('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ username: userName, password: password }),
-        headers: { 'Content-Type': 'application/json' },
+            method: 'POST',
+            body: JSON.stringify({ username: userName, password: password }),
+            headers: { 'Content-Type': 'application/json' },
         });
-    
+
         if (response.ok) {
-        document.location.replace('/profile');
+            document.location.replace('/profile');
         } else {
-        alert('Failed to log in');
+            alert('Failed to log in');
         }
     }
 };
