45ab0e574c186faffd99364b3534f6595677e540
garrett lieb <liebg08@gmail.com>
2024-02-22 22:18:52 -0800

    fixed comments post

--- public/js/newcomment.js
+++ public/js/newcomment.js
@@ -1,7 +1,8 @@
 const newcommentHandler = async (event) => {
     event.preventDefault();
     const comment = document.querySelector('#comment').value.trim();
-    const blog_id = document.querySelector('#blog_id').value.trim();
+    const blog_id = document.querySelector('input[name="post-id"]').value;
+    console.log(comment);
     if (comment) {
         const response = await fetch('/api/comments', {
             method: 'POST',
@@ -12,7 +13,8 @@ const newcommentHandler = async (event) => {
             document.location.replace(`/blogs/${blog_id}`);
         }
         else {
-            alert('Failed to create comment');
+            // redirect to login page
+            document.location.replace('/login');
         }
     }
     console.log('comment:', comment);
