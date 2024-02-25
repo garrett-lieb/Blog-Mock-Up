cf88c4ead1b0e4fc35c05911334634f3dab40ac4
garrett lieb <liebg08@gmail.com>
2024-02-25 14:37:00 -0800

    tree shake

--- public/js/deletepost.js
+++ public/js/deletepost.js
@@ -1,20 +1,20 @@
-const deleteosthandler = async (event) => {
-    event.preventDefault();
-    const title = document.querySelector('#title').value.trim();
-    const content = document.querySelector('#content').value.trim();
-    if (title && content) {
-        const response = await fetch('/api/blogs', {
-            method: 'DELETE',
-            body: JSON.stringify({ title, content }),
-            headers: { 'Content-Type': 'application/json' },
-        });
-        if (response.ok) {
-            document.location.replace('/profile');
-        }
-        else {
-            alert('Failed to create post');
-        }
-    }
-};
+// const deleteosthandler = async (event) => {
+//     event.preventDefault();
+//     const title = document.querySelector('#title').value.trim();
+//     const content = document.querySelector('#content').value.trim();
+//     if (title && content) {
+//         const response = await fetch('/api/blogs', {
+//             method: 'DELETE',
+//             body: JSON.stringify({ title, content }),
+//             headers: { 'Content-Type': 'application/json' },
+//         });
+//         if (response.ok) {
+//             document.location.replace('/profile');
+//         }
+//         else {
+//             alert('Failed to create post');
+//         }
+//     }
+// };
 
-document.querySelector('.delete-form').addEventListener('delete', newposthandler);
+// document.querySelector('.delete-form').addEventListener('delete', newposthandler);
--- public/js/editpost.js
+++ public/js/editpost.js
@@ -1,20 +1,20 @@
-const editposthandler = async (event) => {
-    event.preventDefault();
-    const title = document.querySelector('#title').value.trim();
-    const content = document.querySelector('#content').value.trim();
-    if (title && content) {
-        const response = await fetch('/api/blogs', {
-            method: 'DELETE',
-            body: JSON.stringify({ title, content }),
-            headers: { 'Content-Type': 'application/json' },
-        });
-        if (response.ok) {
-            document.location.replace('/profile');
-        }
-        else {
-            alert('Failed to create post');
-        }
-    }
-};
+// const editposthandler = async (event) => {
+//     event.preventDefault();
+//     const title = document.querySelector('#title').value.trim();
+//     const content = document.querySelector('#content').value.trim();
+//     if (title && content) {
+//         const response = await fetch('/api/blogs', {
+//             method: 'DELETE',
+//             body: JSON.stringify({ title, content }),
+//             headers: { 'Content-Type': 'application/json' },
+//         });
+//         if (response.ok) {
+//             document.location.replace('/profile');
+//         }
+//         else {
+//             alert('Failed to create post');
+//         }
+//     }
+// };
 
-document.querySelector('.edit-form').addEventListener('edit', editposthandler);
+// document.querySelector('.edit-form').addEventListener('edit', editposthandler);
